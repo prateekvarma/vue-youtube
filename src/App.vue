33d87fcb9f2a3e007080305176009c3c4da67ca6
Prateek Varma <prtk.varma@gmail.com>
2022-01-18 21:17:21 -0600

    took the data from the emit function on child component, recieved on the parent, and console logged

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <SearchBar></SearchBar>
+    <SearchBar v-on:termChange="onTermChange"></SearchBar>
 </template>
 
 <script>
@@ -8,6 +8,12 @@ export default {
     name: 'App',
     components: {
         SearchBar
+    },
+    methods: {
+        //the searchTerm below is the 2nd argument from the 'onInput' function in SearchBar.vue
+        onTermChange(searchTerm) {
+            console.log(searchTerm);
+        }
     }
 };
 </script>
