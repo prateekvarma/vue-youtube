c38b69ce8a837eadae7f46d9fe41d53ee0df21df
Prateek Varma <prtk.varma@gmail.com>
2022-01-21 15:29:41 -0600

    recieve the event in the parent(VideoList) and emitting it further to it's own parent(app comoponent)

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -4,6 +4,7 @@
         v-for="(video, index) in videos" 
         v-bind:video="video"
         :key="index"
+        @videoSelect="onVideoSelect"
         >
         </VideoListItem>
     </ul>
@@ -16,7 +17,14 @@ export default {
     components: {
         VideoListItem
     },
-    props: ['videos']
+    props: ['videos'],
+    methods: {
+        onVideoSelect(video) {
+            //Note that the 'videoSelect' is not the same as the @videoSelect above, because it is a completely new event variable that is being emitted to it's own parent component
+            //Also, the 'video' variable does not need the 'this' prefix, because it is not coming from props, but rather just being passed from it's child component.
+            this.$emit('videoSelect', video);
+        }
+    }
 };
 </script>
 
