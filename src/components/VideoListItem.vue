a9028d29586a7679181c4364a6cff366c63e398c
Prateek Varma <prtk.varma@gmail.com>
2022-01-21 13:55:56 -0600

    on click event on the specific video, which emits an event to the parent component for it to play the video

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,7 @@
 <template>
-    <li>
+    /* Below the HTML click event is triggered, which runs the 'onVideoSelect' method, which in turn sends the particular video to be played - by emitting it as an event to it's parent component */
+    
+    <li @click="onVideoSelect">
         <img v-bind:src="video.snippet.thumbnails.default.url" />
         {{ video.snippet.title }}
     </li>
@@ -8,6 +10,11 @@
 <script>
     export default {
         name: 'VideoListItem',
-        props: ['video']
+        props: ['video'],
+        methods: {
+            onVideoSelect() {
+                this.$emit('videoSelect', this.video);
+            }
+        }
     }
 </script>
