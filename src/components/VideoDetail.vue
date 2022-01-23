5dabb942f1c9ff2e3d8f0069a71ec44bbadd78a7
Prateek Varma <prtk.varma@gmail.com>
2022-01-23 15:07:46 -0600

    used youtube iframe to dynamically embed the video

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
     <div v-if="video">
+        <div>
+            <iframe v-bind:src="videoUrl" />
+        </div>
         <h3>{{ video.snippet.title }}</h3>
         <p>{{ video.snippet.description }}</p>
     </div>
@@ -8,6 +11,13 @@
 <script>
 export default {
     name: 'VideoDetail',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        videoUrl() {
+            //the below 'this.video.id.videoId' comes from the prop, whose object structure can be checked from the XHR request on the network tab on browser dev tools
+            const videoId = this.video.id.videoId;
+            return 'https://www.youtube.com/embed/' + videoId;
+        }
+    }
 }
 </script>
